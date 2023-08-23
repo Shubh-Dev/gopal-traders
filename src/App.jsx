1e7d0e61ec5b09f4565b9ef2dcc38220df6d8f12
Shubh <shubh.scb@gmail.com>
2023-08-23 22:02:21 +0530

    feat: footer add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from './components/Header';
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
       <Header />
-      <div className="flex justify-center items-center h-screen"></div>
+      <div className="flex justify-center items-center h-screen">
+
+      </div>
+      <Footer />
     </div>
   );
 }
