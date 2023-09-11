d497624579b5f1d461b4e95d6022d80ed498418a
Shubh <shubh.scb@gmail.com>
2023-09-11 21:52:14 +0530

    feat: our story in progress

---
+++ src/components/parts/GalleryImage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const GalleryImage = ({ image }) => {
+  return (
+    <div className="w-16 rounded-md">
+      <img
+        src={image}
+        alt="gallery"
+        className="w-full h-full object-cover object-center rounded-md"
+      />
+    </div>
+  );
+}
+
+export default GalleryImage;
